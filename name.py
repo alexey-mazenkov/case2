5baef305eae6a98d85993184410ae00a94495950
alexey-mazenkov <mazenkovof@gmail.com>
2019-09-17 11:43:07 +0700

    Set the tasks

--- name.py
+++ name.py
@@ -6,18 +6,39 @@
 
 import turtle
 
-def triangle():
-    # TODO: (Kravtsov) - нарисуй треугольник
-
-
 def square():
     # TODO: (Mikhailov) - нарисуй квадрат
-
+    pass
 
 def circle():
-    # TODO: (Mazenkov) - нарисовать круг
-    
+    # TODO: (Mikhailov) - нарисовать круг
+    pass
+
+def figure1():
+    # TODO: (Mikhailov) - нарисуй фигуру
+    pass
+
+def figure2():
+    # TODO: (Mikhailov) - нарисуй фигуру 2, чтобы одна сторона с figure1 была одинаковой
+    pass
+
+def figure3():
+    # TODO: (Kravtsov) - нарисуй произвольную фигуру 4
+    pass
+
+def figure4():
+    # TODO: (Kravtsov) - нарисуй произвольную фигуру 4, чтобы одна сторона с figure3 была одинаковой
+    pass
+
+def triangle():
+    # TODO: (Kravtsov) - нарисуй треугольник
+    pass
+
+def rectangle():
+    # TODO: (Kravtsov) - нарисуй прямоугольник
+    pass
 
 def main():
     # TODO: (Mazenkov)
     # Main function
+    pass
