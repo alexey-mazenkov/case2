3cbd6dd28e03bba338fcf2f62c38ccb047469160
alexey-mazenkov <mazenkovof@gmail.com>
2019-09-17 11:26:42 +0700

    Goals

--- name.py
+++ name.py
@@ -1,2 +1,20 @@
 # нарисовать фигуры
 # размер окна 1000*1000
+# Developers: A.Mazenkov
+#             A.Mikhailov
+#             K.Kravtsov
+
+def triangle():
+    # TODO: (Kravtsov) - нарисуй треугольник
+
+
+def square():
+    # TODO: (Mikhailov) - нарисуй квадрат
+
+
+def circle():
+    # TODO: (Mazenkov) - нарисовать круг
+
+def main():
+    # TODO: (Mazenkov)
+    # Main function
