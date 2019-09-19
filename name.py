1cd90fc06a78e04c205a9aea9e5d0cfd5214fb7e
alexey-mazenkov <mazenkovof@gmail.com>
2019-09-19 21:06:32 +0700

    Import math, cleaned the code and fixed a right triangle.

--- name.py
+++ name.py
@@ -5,122 +5,128 @@
 #             K.Kravtsov
 
 import turtle
+import math
 
+t = turtle
+m = math
 def square(x, y, a):
     # TODO: (Mikhailov) - нарисуй квадрат
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.up
-    turtle.exitonclick()
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(90)
+    t.forward(a)
+    t.right(90)
+    t.forward(a)
+    t.right(90)
+    t.forward(a)
+    t.right(90)
+    t.up
 
 def circle(x, y, a):
     # TODO: (Mikhailov) - нарисовать круг
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.circle(a)
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.circle(a)
 
 
 def figure1(x, y, a):
     # TODO: (Mikhailov) - нарисуй фигуру
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(140)
-    turtle.forward(a)
-    turtle.right(40)
-    turtle.forward(a)
-    turtle.right(140)
-    turtle.forward(a)
-    turtle.right(40)
-    turtle.up()
+    # Ромб.
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(140)
+    t.forward(a)
+    t.right(40)
+    t.forward(a)
+    t.right(140)
+    t.forward(a)
+    t.right(40)
+    t.up()
 
 
 def figure2(x, y, a):
     # TODO: (Mikhailov) - нарисуй фигуру 2, чтобы одна сторона с figure1 была одинаковой
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(120)
-    turtle.forward(a)
-    turtle.right(60)
-    turtle.forward(a)
-    turtle.right(120)
-    turtle.forward(a)
-    turtle.up()
+    # Параллелограмм
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(120)
+    t.forward(a)
+    t.right(60)
+    t.forward(a)
+    t.right(120)
+    t.forward(a)
+    t.up()
 
 
 def figure3(x, y, a):
     # TODO: (Kravtsov) - нарисуй произвольную фигуру 4
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(72)
-    turtle.forward(a)
-    turtle.right(72)
-    turtle.forward(a)
-    turtle.right(72)
-    turtle.forward(a)
-    turtle.right(72)
-    turtle.forward(a)
-    turtle.right(72)
-    turtle.up()
+    # Пятиугольник
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(72)
+    t.forward(a)
+    t.right(72)
+    t.forward(a)
+    t.right(72)
+    t.forward(a)
+    t.right(72)
+    t.forward(a)
+    t.right(72)
+    t.up()
 
 
-def figure4(x, y, a, b, c):
+def figure4(x, y, a, c):
     # TODO: (Kravtsov) - нарисуй произвольную фигуру 4, чтобы одна сторона с figure3 была одинаковой
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(b)
-    turtle.right(120)
-    turtle.forward(c)
-    turtle.right(150)
-    turtle.up()
+    # Прямоугольный треугольник
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(135)
+    t.forward(m.sqrt(a ** 2 + c ** 2))
+    t.right(135)
+    t.forward(c)
+    t.right(135)
+    t.up()
 
 
 def triangle(x, y, a):
     # TODO: (Kravtsov) - нарисуй равносторонний треугольник
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(120)
-    turtle.forward(a)
-    turtle.right(120)
-    turtle.forward(a)
-    turtle.right(120)
-    turtle.up()
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(120)
+    t.forward(a)
+    t.right(120)
+    t.forward(a)
+    t.right(120)
+    t.up()
 
 
 def rectangle(x, y, a, b):
     # TODO: (Kravtsov) - нарисуй прямоугольник
-    turtle.up()
-    turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(b)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(b)
-    turtle.right(90)
-    turtle.up()
+    t.up()
+    t.setposition(x, y)
+    t.down()
+    t.forward(a)
+    t.right(90)
+    t.forward(b)
+    t.right(90)
+    t.forward(a)
+    t.right(90)
+    t.forward(b)
+    t.right(90)
+    t.up()
 
 
 def main():
@@ -129,11 +135,8 @@ def main():
     :return: None
     '''
     # рисунок 1.
-    turtle.hideturtle()
-    circle(100,100,100)
-    triangle(0, 0, 20, 20, 20)
-    figure2(-100, -100, 20)
-    figure1(-40, 0, 30)
+    t.hideturtle()
+    figure4(40, 70, 70, 70)
     turtle.done()
 
 
